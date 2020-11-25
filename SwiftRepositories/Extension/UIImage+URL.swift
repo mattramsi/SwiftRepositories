e10c61a370e6f2c54c61b9d25dae5afd59230bd3
Matheus <matheusramossilva999@gmail.com>
2020-11-25 00:00:49 -0300

    ajustes

--- SwiftRepositories/Extension/UIImage+URL.swift
+++ SwiftRepositories/Extension/UIImage+URL.swift
@@ -13,6 +13,7 @@ extension UIImage {
 
     public static func loadFrom(url: URL, completion: @escaping (_ image: UIImage?) -> ()) {
         DispatchQueue.global().async {
+            
             if let data = try? Data(contentsOf: url) {
                 DispatchQueue.main.async {
                     completion(UIImage(data: data))
