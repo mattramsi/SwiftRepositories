9d13766c70ae1c07dcf87926dbed82cfcd53fe03
Matheus <matheusramossilva999@gmail.com>
2020-11-25 00:18:35 -0300

    ajustes

---
+++ Pods/Target Support Files/Pods-SwiftRepositories-RepositoriesTest/Pods-SwiftRepositories-RepositoriesTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftRepositories_RepositoriesTest : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftRepositories_RepositoriesTest
+@end
